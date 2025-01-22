f7601b72a5fb6a96d759c1277ec338016abb1020
cophilot <philxsb@gmail.com>
2025-01-22 17:20:15 +0100

    added index btn

--- src/games/everdell-duo/View.tsx
+++ src/games/everdell-duo/View.tsx
@@ -2,5 +2,25 @@ import GameView from '../../components/GameView';
 import getDefinition from './definition';
 
 export default function EverdellDuoView() {
-    return <GameView definition={getDefinition()} />;
+	return (
+		<GameView
+			definition={getDefinition()}
+			afterTabelElement={<IndexButton />}
+		/>
+	);
 }
+
+const IndexButton = () => {
+	return (
+		<a
+			href="https://cdn.shopify.com/s/files/1/0559/8245/6947/files/Duo-Complete_Card_Index-V03-Web.pdf?v=1723217153"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<button className="btn selected nav-btn print-hide">
+				<i className="bi bi-list-columns-reverse"></i>
+				Index
+			</button>
+		</a>
+	);
+};
