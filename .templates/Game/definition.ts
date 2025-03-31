d93cc87a9211f580a543592b15093569be0a1317
cophilot <philxsb@gmail.com>
2025-03-31 17:14:10 +0200

    template enhancement

--- .templates/Game/definition.ts
+++ .templates/Game/definition.ts
@@ -1,15 +1,21 @@
 import { GameDef } from '../../core/types/GameDef';
 import { WinMode } from '../../core/types/WinMode';
 
+/**
+ * This is the definition for the $$name.p$$ game.
+ * @author $$git-name$$
+ * @version 1.0.0
+ * @created $$year$$-$$month$$-$$day$$
+ */
 export default function getDefinition(): GameDef {
 	return {
 		title: '$$name.p$$',
-		//url: '',
-		//rulesUrl: '',
-		//bgColor: '#fff',
-		//fontColor: '#000',
-		//primaryColor: '#fff',
-		//secondaryColor: '#000',
+		url: '$$url$$',
+		rulesUrl: '$$rulesUrl$$',
+		bgColor: '$$backgroundColor$$',
+		fontColor: '$$fontColor$$',
+		primaryColor: '$$primaryColor$$',
+		secondaryColor: '$$secondaryColor$$',
 		//fontFamily: FontUtils.PLAYFUL,
 		//stripeColor: '#000',
 		playerSizes: [1, 2, 3, 4],
