6c05e475e93d80d865eb25e10fb49b00ff36c95b
cophilot <philxsb@gmail.com>
2025-04-07 16:50:56 +0200

    extended search bar

--- src/components/GameSearch/GameSearch.tsx
+++ src/components/GameSearch/GameSearch.tsx
@@ -65,5 +65,38 @@ function filterGameNames(gameNames: string[], searchString: string): string[] {
 }
 
 function normalizeString(str: string) {
-	return str.toLowerCase().replace(/[^a-z0-9]/g, '');
+	return replaceNumbersWithWords(str.toLowerCase().replace(/[^a-z0-9]/g, ''));
+}
+
+function replaceNumbersWithWords(str: string): string {
+	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach((num) => {
+		const regex = new RegExp(num.toString(), 'g');
+		str = str.replace(regex, numberToVerbalString(num));
+	});
+	return str;
+}
+
+function numberToVerbalString(num: number): string {
+	switch (num) {
+		case 1:
+			return 'one';
+		case 2:
+			return 'two';
+		case 3:
+			return 'three';
+		case 4:
+			return 'four';
+		case 5:
+			return 'five';
+		case 6:
+			return 'six';
+		case 7:
+			return 'seven';
+		case 8:
+			return 'eight';
+		case 9:
+			return 'nine';
+		default:
+			return '';
+	}
 }
