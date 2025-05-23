1b794bf835e6127efe549454b3ccb872ad920590
cophilot <philxsb@gmail.com>
2025-05-23 10:59:23 +0200

    Added randomizer link

--- src/games/seti/View.tsx
+++ src/games/seti/View.tsx
@@ -2,5 +2,25 @@ import GameView from '../../components/GameView';
 import getDefinition from './definition';
 
 export default function SetiView() {
-	return <GameView definition={getDefinition()} />;
+	return (
+		<GameView
+			definition={getDefinition()}
+			afterTabelElement={<IndexButton />}
+		/>
+	);
 }
+
+const IndexButton = () => {
+	return (
+		<a
+			href="https://seti-solarsystem.czechgames.com/"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<button className="btn selected nav-btn print-hide">
+				<i className="bi bi-globe-americas"></i>
+				Randomizer
+			</button>
+		</a>
+	);
+};
