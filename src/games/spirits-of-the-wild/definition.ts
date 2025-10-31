31ecd5201dd4b49a6cf06863ec9ef7ddad2f1f3c
cophilot <philxsb@gmail.com>
2025-10-31 19:26:13 +0100

    fixed bg color

--- src/games/spirits-of-the-wild/definition.ts
+++ src/games/spirits-of-the-wild/definition.ts
@@ -11,7 +11,7 @@ export default function getDefinition(): GameDef {
 		url: 'https://boardgamegeek.com/boardgame/256606/spirits-of-the-wild',
 		rulesUrl:
 			'https://s3.amazonaws.com/geekdo-files.com/bgg222578?response-content-disposition=inline%3B%20filename%3D%22SpiritsOfTheWild-Rules-DOM-03-Digital.pdf%22&response-content-type=application%2Fpdf&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJYFNCT7FKCE4O6TA%2F20241019%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241019T181306Z&X-Amz-SignedHeaders=host&X-Amz-Expires=120&X-Amz-Signature=93a304c53762c1f6cd4d9eedfbccf51a7cd9487d4b5d0697fedd401a37473efc',
-		bgColor: '#13103b',
+		bgColor: '#0b0738',
 		fontColor: '#fff',
 		primaryColor: '#00d1f7',
 		secondaryColor: '#fff',
