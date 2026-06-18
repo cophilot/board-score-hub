5c4fd4c0d4e79f05967263f72add313de4cb2e22
cophilot <philxsb@gmail.com>
2026-06-18 16:58:51 +0200

    Fix formatting

--- src/games/bonsai/main.ts
+++ src/games/bonsai/main.ts
@@ -2,7 +2,7 @@ import getDefinition from './definition';
 import BonsaiView from './View';
 
 const Bonsai = {
-    view: BonsaiView,
-    definition: getDefinition(),
+	view: BonsaiView,
+	definition: getDefinition(),
 };
 export default Bonsai;
