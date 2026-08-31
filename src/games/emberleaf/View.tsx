dfdb47f4add596cdd0434541d0633c84467e39c3
cophilot <philxsb@gmail.com>
2026-08-31 08:35:12 +0200

    Added randomizer button

--- src/games/emberleaf/View.tsx
+++ src/games/emberleaf/View.tsx
@@ -2,5 +2,24 @@ import GameView from '../../components/GameView';
 import getDefinition from './definition';
 
 export default function EmberleafView() {
-	return <GameView definition={getDefinition()} />;
+	return (
+		<GameView
+			definition={getDefinition()}
+			beforeTableElement={<RandomizerBtn />}
+		/>
+	);
 }
+const RandomizerBtn = () => {
+	return (
+		<a
+			href="https://cophilot.github.io/emberleaf-randomizer/"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<button className="btn selected nav-btn print-hide">
+				<i className="bi bi-shuffle"></i>
+				Randomizer
+			</button>
+		</a>
+	);
+};
